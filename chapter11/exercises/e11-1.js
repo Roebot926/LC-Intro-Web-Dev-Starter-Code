ea5d7200893aae8a103a434cf14a660976f6016f
Jordan <138933858+Roebot926@users.noreply.github.com>
2023-07-12 10:57:29 -0400

    hello

--- chapter11/exercises/e11-1.js
+++ chapter11/exercises/e11-1.js
@@ -8,6 +8,16 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. */
 
+let randomFunction = function(str){
+    if (typeof str === "number"){
+      return str*3;
+    } else if(typeof str === "string"){
+      return "Arrr!";
+    } else{
+      return str;
+    }
+  }
+console.log(randomFunction("10"))
 
 
 /* Use your fuction and the map method to change an array  as follows:
